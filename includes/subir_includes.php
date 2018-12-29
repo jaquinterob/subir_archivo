1f612713299a464a8b488a370c6a4a5efdf84856
John Alexander Quintero <quinteroberrio@gmail.com>
2018-12-29 18:00:39 -0500

    se agrega .gitignore

--- includes/subir_includes.php
+++ includes/subir_includes.php
@@ -1,8 +1,5 @@
 <?php
   $nombre_temporal=$_FILES['archivo']['tmp_name'];
   $nombre=$_FILES['archivo']['name'];
-  echo $nombre_temporal.'<br><br>';
-  echo $nombre;
   move_uploaded_file($nombre_temporal,'../archivos/'.$nombre);
-
 ?>
